da8dc47acb5ba8fa7f9d7b093c8c3016048a0f7d
ibrahim-ibrahim <folahanmi0001@gmail.com>
2025-10-21 00:41:47 +0100

    feat: add increment_any and decrement instructions

--- programs/sol-counter/src/lib.rs
+++ programs/sol-counter/src/lib.rs
@@ -21,8 +21,8 @@ pub mod sol_counter {
         Ok(())
     }
 
-    pub fn increment(ctx: Context<Increment>) -> Result<()> {
-        msg!("Incrementing...");
+    pub fn increment_one(ctx: Context<Update>) -> Result<()> {
+        msg!("Incrementing 1...");
 
         let counter = &mut ctx.accounts.counter;
         msg!("Previous count: {}", counter.count);
@@ -33,7 +33,62 @@ pub mod sol_counter {
             .checked_add(1)
             .expect("Error adding 1 to count");
 
-        msg!("Increment complete!");
+        msg!("Increment 1 complete!");
+        msg!("Current count: {}", counter.count);
+
+        Ok(())
+    }
+
+    pub fn decrement_one(ctx: Context<Update>) -> Result<()> {
+        msg!("Decrementing 1...");
+
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous count: {}", counter.count);
+
+        // Used `checked_sub`` instead of `-=` for safety
+        counter.count = counter
+            .count
+            .checked_sub(1)
+            .expect("Error removing 1 from count");
+
+        msg!("Decrement 1 complete!");
+        msg!("Current count: {}", counter.count);
+
+        Ok(())
+    }
+    pub fn increment_any(ctx: Context<Update>, value: u64) -> Result<()> {
+        msg!("Incrementing {}...", value);
+
+        require!(value > 0, MyError::InvalidCountValue);
+
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous count: {}", counter.count);
+
+        counter.count = counter
+            .count
+            .checked_add(value)
+            .expect(&format!("Error adding {} to count", value));
+
+        msg!("Increment {} complete!", value);
+        msg!("Current count: {}", counter.count);
+
+        Ok(())
+    }
+
+    pub fn decrement_any(ctx: Context<Update>, value: u64) -> Result<()> {
+        msg!("Decrementing {}...", value);
+
+        require!(value > 0, MyError::InvalidCountValue);
+
+        let counter = &mut ctx.accounts.counter;
+        msg!("Previous count: {}", counter.count);
+
+        counter.count = counter
+            .count
+            .checked_sub(value)
+            .expect(&format!("Error removing {} from count", value));
+
+        msg!("Decrement {} complete!", value);
         msg!("Current count: {}", counter.count);
 
         Ok(())
@@ -45,28 +100,33 @@ pub mod sol_counter {
 pub struct Initialize<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
-
     #[account(
         init,
         payer = user,
         space = Counter::SIZE,
     )]
     pub counter: Account<'info, Counter>,
-
     pub system_program: Program<'info, System>,
 }
 
 #[derive(Accounts)]
-pub struct Increment<'info> {
+pub struct Update<'info> {
     #[account(mut)]
     pub counter: Account<'info, Counter>,
 }
 
-// State
+/// State
 #[account]
 pub struct Counter {
     pub count: u64,
 }
 impl Counter {
     pub const SIZE: usize = 8 + 8;
+}
+
+/// Errors
+#[error_code]
+pub enum MyError {
+    #[msg("Invalid count value")]
+    InvalidCountValue,
 }
--- tests/sol-counter.ts
+++ tests/sol-counter.ts
@@ -2,6 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Keypair, SystemProgram } from "@solana/web3.js";
 import { SolCounter } from "../target/types/sol_counter";
+import { assert } from "chai";
 
 describe("sol-counter", () => {
   let provider = anchor.AnchorProvider.env();
@@ -9,10 +10,12 @@ describe("sol-counter", () => {
 
   const program = anchor.workspace.solCounter as Program<SolCounter>;
 
-  // Generate keypair for counter
   const user_wallet = provider.wallet.publicKey;
   const payer = provider.wallet.payer; // payer = user
   const counter = Keypair.generate();
+
+  const incrementNum = 20;
+  const decrementNum = 8;
 
   it("Initialize counter", async () => {
     const tx = await program.methods
@@ -27,21 +30,76 @@ describe("sol-counter", () => {
     console.log("Initialize counter: Txn signature", tx);
   });
 
-  it("Increment counter", async () => {
+  it("Increment counter by 1", async () => {
     const tx = await program.methods
-      .increment()
+      .incrementOne()
       .accounts({
         counter: counter.publicKey,
       })
       .signers([payer])
       .rpc();
 
-    console.log("Increment: Txn signature", tx);
+    console.log("Increment 1: Txn signature", tx);
 
     // Fetch the counter account
-    const counterAccont = await program.account.counter.fetch(counter.publicKey);
-    console.log("Count:", counterAccont.count.toNumber());
+    const counterAccount = await program.account.counter.fetch(counter.publicKey);
+    const countNumber = counterAccount.count.toNumber();
+    console.log("Count:", countNumber);
+
+    assert(countNumber === 1, "Expected to increment by 1");
   });
 
+  it("Decrement counter by 1", async () => {
+    const tx = await program.methods
+      .decrementOne()
+      .accounts({
+        counter: counter.publicKey,
+      })
+      .signers([payer])
+      .rpc();
 
+    console.log("Decrement 1: Txn signature", tx);
+
+    const counterAccount = await program.account.counter.fetch(counter.publicKey);
+    const countNumber = counterAccount.count.toNumber();
+    console.log("Count:", countNumber);
+
+    assert(countNumber === 0, "Expected to decrement by 1");
+  });
+
+  it(`Increment counter by ${incrementNum}`, async () => {
+    const tx = await program.methods
+      .incrementAny(new anchor.BN(incrementNum))
+      .accounts({
+        counter: counter.publicKey,
+      })
+      .signers([payer])
+      .rpc();
+
+    console.log(`Increment ${incrementNum}: Txn signature`, tx);
+
+    const counterAccount = await program.account.counter.fetch(counter.publicKey);
+    const countNumber = counterAccount.count.toNumber();
+    console.log("Count:", countNumber);
+
+    assert(countNumber === 20, `Expected to increment by ${incrementNum}`);
+  });
+
+  it(`Decrement counter by ${decrementNum}`, async () => {
+    const tx = await program.methods
+      .decrementAny(new anchor.BN(decrementNum))
+      .accounts({
+        counter: counter.publicKey,
+      })
+      .signers([payer])
+      .rpc();
+
+    console.log(`Decrement ${decrementNum}: Txn signature`, tx);
+
+    const counterAccount = await program.account.counter.fetch(counter.publicKey);
+    const countNumber = counterAccount.count.toNumber();
+    console.log("Count:", countNumber);
+
+    assert(countNumber === 12, `Expected to decrement by ${decrementNum}`);
+  });
 });
